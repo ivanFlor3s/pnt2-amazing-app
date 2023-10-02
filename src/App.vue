78545ce4b18a7ca1aebe89747989b47e28707ab8
ivanFlor3s <ifloresmanc96@gmail.com>
2023-10-01 21:11:18 -0300

    Componente image nasa funcionando con props

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import ImagenNasa from './components/ImagenNasa.vue'
 </script>
 
 <template>
-  <h1>Hola mundo</h1>
+  <div class="d-flex">
+    <ImagenNasa titulo="Alta imagen" fecha="1996" imagen="https://apod.nasa.gov/apod/image/2206/V838Mon_Hubble_960.jpg"></ImagenNasa>
+    <!-- <ImagenNasa></ImagenNasa> -->
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/ImagenNasa.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="card"
+    :style="{ background: 'url(' + imagen + ') no-repeat center center fixed'}"
+  >
+    <div class="card-body text-white">
+      <h1>{{ titulo }}</h1>
+      <p>Fecha de foto tomada: {{ fecha }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  imagen: {
+    type: String,
+    required: true
+  },
+  titulo: {
+    type: String,
+    required: true
+  },
+  fecha: {
+    type: String,
+    required: true
+  },    
+  descripcion: {
+    type: String,
+    required: true
+  },
+  mostrarBoton: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
