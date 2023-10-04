b1ab554919acbbfbffc95a6c35ed1e521cf38306
ADCarrozzo <115608798+ADCarrozzo@users.noreply.github.com>
2023-10-03 22:39:40 -0300

    filtro de videos

--- src/utils/nasa-fetch.ts
+++ src/utils/nasa-fetch.ts
@@ -4,5 +4,7 @@ export const fetchImagesFromNasa = async () => {
   const API_KEY = 'J8rPWR4nzqSIb2xCJpsvl7E3p0rMdrcsqbkZSE3t'
   const result = await axios.get('https://api.nasa.gov/planetary/apod', { params: { count: 20, api_key: API_KEY, thumbs: 'false' } })
   console.log(result)
-  return result.data;
+  return result.data.filter((image) => {
+    return image.media_type == "image"
+  });
 }
