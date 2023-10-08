80e5a151f4bfbd767fef0dd71a413e1cf9cff58e
victoriaMartine <129807753+victoriaMartine@users.noreply.github.com>
2023-10-08 20:55:07 -0300

    creacion rutas login y game

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import AppLogin from '../pages/AppLogin.vue'
+import AppGame from '../pages/AppGame.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [ { path: '/login', component: AppLogin },
+  { path: '/game', component: AppGame }
+]
 })
 
 export default router
