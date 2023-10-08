eab4d6702e233993ee0a54ba6dec25643771f061
ivanFlor3s <ifloresmanc96@gmail.com>
2023-10-07 21:58:44 -0300

    button option + text arreglos

--- src/components/ImagenNasa.vue
+++ src/components/ImagenNasa.vue
@@ -1,12 +1,19 @@
 <template>
   <div
-    class="card nasa-image" 
+    class="nasa-image" 
     :style="{ background: 'no-repeat center/cover url(' + imagen + ')', minHeight: '100vh'}"
 
   >
-    <div class="card-body text-white">
-      <h1>{{ titulo }}</h1>
-      <p>Fecha de foto tomada: {{ fecha }}</p>
+    <div class="p-3 text-white">
+      <div class="d-flex flex-column justify-content-between h-100">
+        <article class="p-2 rounded bg-text text-balance">
+          <h1>{{ titulo }}</h1>
+          <p>Fecha de foto tomada: {{ fecha }}</p>
+        </article>
+        <section class="mt-3">
+          <button class="btn btn-primary">Opcion</button>
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -39,5 +46,8 @@ defineProps({
 <style>
 .nasa-image{
    max-width: 50%;
+}
+.bg-text{
+  background-color: rgba(0,0,0,0.3);
 }
 </style>
