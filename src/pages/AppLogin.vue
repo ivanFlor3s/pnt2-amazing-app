61e854c53284be5c08a8ef2042d2e8d96c13009b
victoriaMartine <129807753+victoriaMartine@users.noreply.github.com>
2023-10-08 21:41:48 -0300

    login complketo

--- src/pages/AppLogin.vue
+++ src/pages/AppLogin.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Login</h1>
+    <div class="container">
+        <h1>Identificate</h1>
+        <form  @submit.prevent>
+            <div class="mb-3">
+                <label for="exampleInputText" class="form-label"> Nombre Usuario</label>
+                <input :class="{'is-invalid':nombreUser.length < 4}" v-model="nombreUser" type="text" class="form-control" id="exampleInputText">
+            </div>
+            <button type="submit" @click="submit()" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </template>
+<script setup>
+import { ref } from 'vue';
+import router from '@/router'
+ const nombreUser = ref('')
+ const submit = ()=>{
+    if(nombreUser.value.length > 4){
+        router.push('game')
+    }
+ }
+</script>
