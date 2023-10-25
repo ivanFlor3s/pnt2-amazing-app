889d28e16031b4f56251088259dab6440512a281
victoriaMartine <129807753+victoriaMartine@users.noreply.github.com>
2023-10-24 22:02:48 -0300

    nueva ruta resultado

---
+++ src/pages/AppResult.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="background: no-repeat center/cover url( 'https://media.giphy.com/media/biHH61yRTp0RZGNvxi/giphy.gif' ); min-height: 100vh">
+       <div class="d-flex h-100" >
+        <div class="custom-card " style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Ganaste!!</h5>
+                <ListaUsuarios :mostrar-barra="false"></ListaUsuarios>
+                <a href="#" class="btn btn-primary">Volver a jugar</a>
+            </div>
+        </div>
+       </div>
+       
+    </div> 
+</template>
+
+<script setup>
+import ListaUsuarios from '../components/ListaUsuarios.vue';
+
+</script>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppLogin from '../pages/AppLogin.vue'
 import AppGame from '../pages/AppGame.vue'
+import AppResult from '../pages/AppResult.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [ { path: '/login', component: AppLogin },
-  { path: '/game', component: AppGame }
+  { path: '/game', component: AppGame },
+  { path: '/result', component: AppResult }
 ]
 })
 
