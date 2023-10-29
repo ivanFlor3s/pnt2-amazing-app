2d9b6b85856c839169a3a64455dd6e2b1896a830
ivanFlor3s <ifloresmanc96@gmail.com>
2023-10-29 20:26:55 -0300

    volver a jugar button: reset store + navigate

--- src/pages/AppResult.vue
+++ src/pages/AppResult.vue
@@ -4,7 +4,7 @@
             <div class="p-3">
                 <h1 class="text-center">Ganaste!!!</h1>
                 <ListaUsuarios :mostrar-barra="false"></ListaUsuarios>
-                <a href="#" class="btn btn-primary mt-3">Volver a jugar</a>
+                <a  @click="volverAJugar()" class="btn btn-primary mt-3">Volver a jugar</a>
             </div>
         </div>
     </div> 
@@ -12,6 +12,15 @@
 
 <script setup>
 import ListaUsuarios from '../components/ListaUsuarios.vue';
+import router from '@/router'
+import { appStateStore } from '../stores/appState';
+
+const appStore = appStateStore()
+
+function volverAJugar(){
+    appStore.reiniciarPuntos();
+    router.push('game')
+}
 
 </script>
 <style></style>
