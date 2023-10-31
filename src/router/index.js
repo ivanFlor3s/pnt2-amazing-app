47cc503714597ed0f6b063ef340d6bf819fcfde2
ivanFlor3s <ifloresmanc96@gmail.com>
2023-10-31 19:40:41 -0300

    format routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AppLogin from '../pages/AppLogin.vue'
 import AppGame from '../pages/AppGame.vue'
 import AppResult from '../pages/AppResult.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [ { path: '/login', component: AppLogin },
-  { path: '/game', component: AppGame },
-  { path: '/result', component: AppResult }
-]
+  routes: [
+    { path: '/login', component: AppLogin },
+    { path: '/game', component: AppGame },
+    { path: '/result', component: AppResult }
+  ]
 })
 
 export default router
