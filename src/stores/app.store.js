243c8f3617d5875adf8c52176ad65e189e156f99
ivanFlor3s <ifloresmanc96@gmail.com>
2023-12-12 00:00:40 -0300

    save role in app store

--- src/stores/app.store.js
+++ src/stores/app.store.js
@@ -1,25 +1,28 @@
 import { defineStore } from 'pinia'
 
-
-
 export const appStore = defineStore('app', {
-    state: () => ({
-        user: null,
-    }),
-    actions: {
-        setUser({id, name, lastName, email}){
-            this.user = {id, name, lastName, email};
-            sessionStorage.setItem('user', JSON.stringify(this.user)); 
-        },
-        logout(){
-            this.user = null;
-            sessionStorage.removeItem('user');
-        },
-        setUserFromSessionStorage(){
-            const user = sessionStorage.getItem('user');
-            if(user){
-                this.user = JSON.parse(user);
-            }
-        }
+  state: () => ({
+    user: null
+  }),
+  getters: {
+    isAdmin: (state) => {
+      return state.user && state.user.role === 'ADMIN'
     }
+  },
+  actions: {
+    setUser({ id, name, lastName, email, role }) {
+      this.user = { id, name, lastName, email, role }
+      sessionStorage.setItem('user', JSON.stringify(this.user))
+    },
+    logout() {
+      this.user = null
+      sessionStorage.removeItem('user')
+    },
+    setUserFromSessionStorage() {
+      const user = sessionStorage.getItem('user')
+      if (user) {
+        this.user = JSON.parse(user)
+      }
+    }
+  }
 })
