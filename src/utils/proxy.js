332dd86385ae1f7d41bd5317f99ec3d3bf1aadf2
ivanFlor3s <ifloresmanc96@gmail.com>
2023-12-15 20:39:50 -0300

    add authorization headers in update

--- src/utils/proxy.js
+++ src/utils/proxy.js
@@ -3,6 +3,7 @@ const BASE_URL = 'http://localhost:3000/api/v1'
 
 export const login = async (email, password) => {
   const response = await axios.post(BASE_URL + '/auth/login', { email, password })
+  
   return response.data
 }
 
@@ -17,7 +18,10 @@ export const register = async (name, lastName, email, password) => {
 }
 
 export const updateUser = async (id, name, lastName, email) => {
-  const response = await axios.put(BASE_URL + '/users/' + id, { name, lastName, email })
+  const response = await axios.put(BASE_URL + '/users/' + id, { name, lastName, email }, 
+  {
+    headers: { Authorization: sessionStorage.getItem('token') }
+  })
   return response.data
 }
 
