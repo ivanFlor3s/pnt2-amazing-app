77cb77657dc9f706128a012432ccfa72bd46147d
ivanFlor3s <ifloresmanc96@gmail.com>
2023-12-15 22:01:54 -0300

    create user label component

---
+++ src/components/home/UserLabel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-inline">
+    <span class="badge bg-black me-2" v-for="user in game.getUsers" :key="user.id" >
+        <i class="fa-regular fa-user"></i>  
+        {{ user.userName }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { gameStore } from '../../stores/game-state'
+
+const game = gameStore();
+
+</script>
