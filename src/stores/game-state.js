9324f03d661da7d3ad02eade8bf99dda3a4afd44
ivanFlor3s <ifloresmanc96@gmail.com>
2023-12-17 12:46:15 -0300

    current game in game store

--- src/stores/game-state.js
+++ src/stores/game-state.js
@@ -4,16 +4,24 @@ import { defineStore } from 'pinia'
 
 export const gameStore = defineStore('game', {
     state: () => ({
-        users: []
+        users: [],
+        currentGame: null,
     }),
     getters: {
         getUsers(){
             return this.users;
+        },
+        maxScore(state){
+            return state.currentGame?.scoreMax;
         }
     },
     actions: {
         setUsers(users){
             this.users = users;
+        },
+        setCurrentGame(game){
+            console.log('seteo game',game)
+            this.currentGame = game;
         }
     }
 })
